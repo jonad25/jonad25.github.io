4d19268867c3000b5210399397d5ab4a89fcd377
jonad25 <166851900+jonad25@users.noreply.github.com>
2024-05-28 08:54:18 -0400

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -7,11 +7,3 @@ function closeForm() {
   document.getElementById("myForm").style.display = "none";
 }
 
-
-let person = prompt("Please enter your name (first and last)", "");
-let text;
-if (person == null || person == "") {
-  text = "User cancelled the prompt.";
-} else {
-  text = "Hello " + person + "! You have been invited ";
-}
