1270395c8a8e5d4831728f9a49c8ccb8ef0626a5
jonad25 <166851900+jonad25@users.noreply.github.com>
2024-06-10 03:37:06 -0400

    Update myscripts.js

--- myscripts.js
+++ myscripts.js
@@ -3,9 +3,6 @@ function openForm() {
   document.getElementById("myForm").style.display = "block";
 }
 
-function closeForm() {
-  document.getElementById("myForm").style.display = "none";
-}
 
 function testFunction() {
     prompt(document.getElementById("myForm").value;);
